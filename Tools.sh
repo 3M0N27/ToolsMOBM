137024719127176afe085c22b6f5771722535065
3M0N27 <46285777+3M0N27@users.noreply.github.com>
2019-01-13 18:59:59 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,256 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+
+
+echo ""
+echo $red"S"
+sleep 0.2
+clear
+echo $green"SE"
+sleep 0.2
+clear
+echo $yellow"SED"
+sleep 0.2
+clear
+echo $blue"SEDAN"
+sleep 0.2
+clear
+echo $green"SEDANG"
+sleep 0.2
+clear
+echo $red"SEDANG L"
+sleep 0.2
+clear
+echo $purple"SEDANG LO"
+sleep 0.2
+clear
+echo $cyan"SEDANG LOA"
+sleep 0.2
+clear
+echo $purple"SEDANG LOAD"
+sleep 0.2
+clear
+echo $red"SEDANG LOADI"
+sleep 0.2
+clear
+echo $yellow"SEDANG LOADIN"
+sleep 0.2
+clear
+echo $green"SEDANG LOADING...."
+sleep 0.2
+clear
+echo $red"SEDANG LOADING......."
+sleep 0.5
+clear
+echo $YELLOW"WELCOME TO TOOLS Mr.3M0N27"
+sleep 0.6
+clear
+echo $green"SEMOGA MEMBANTU EA MANK:)"
+sleep 0.5
+clear
+echo
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 1.5
+clear
+echo
+echo
+echo $red"==========[Mr.3M0N27]=============[Mr.3M0N27]========="
+echo ""
+echo $yellow"[Author by] = Mr.3M0N27"
+echo $red"[Contact me] = 081799585663"
+echo $purple"[Team] = MOBM(Man Of Black Mask)"
+echo $blue"[Thanks to] = All Members"
+echo $red"[Special Thanks to] = Switywaber,Affan/Nyai,Mr.Deniiganzz"
+echo $green"[Mr.3M0N27]=============[Mr.3M0N27]============[Mr.3M0N27]"
+echo
+echo $red"Welcome :), ini tools dibuat disaat gabut,suntut,kentut.. wkwkwkwk :v"
+echo $yellow"...........----------::::::::----------..............."
+echo $green"............:::::::::::::::.........:::::::::::::...."
+echo $cyan"...........BTW GUA GANZ YAAA:v...................."
+echo $red":::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo $green"+++++++ Silahkan pilih nomer dibawah ini +++++++++++"
+echo ""
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $red"|  [1] SPAM WA BUKALAPAK"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $yellow"|  [2] PRANK DI TELFON TOKOPEDIA"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $green"|  [3]  HACK FACEBOOK MBF"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $purple"|  [4] KALKULATOR"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $blue"|  [5] Game Janda"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $yellow"|  [6] LITESPAM"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $red"|  [7] SANTET ONLINE"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $yellow"|  [8] LiteDDOS"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $blue"|  [0] EXIT"
+echo "[X]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[X]"
+echo $green">>>>>>>>,<<<<<<"
+read -p "√°-[Pilih nomernya]
+root@3M0N27××••>" bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+echo $green"SPAM BUKALAPAK...."
+sleep 3
+apt update && apt upgrade
+pkg install git
+pkg install php
+git clone https://github.com/siputra12/prank
+mv prank$HOME
+cd prank
+php wa.php
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo
+echo $green"PRANK DI TELFON TOKOPEDIA....."
+sleep 1
+apt update && apt upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+mv prank $HOME
+cd prank
+php call.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo $red"Hack metode MBF....."
+sleep 3
+apt update && apt upgrade
+pkg install python2
+pkg install mechanize
+pkg install git
+git clone https://github.com/hnov7/MBF
+mv MBF$HOME
+cd MBF
+python2 mbf.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo $yellow"KALKULATOR........."
+sleep 3
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/3M0N27/kalkulator
+mv kalkulator$HOME
+cd kalkulator
+python2 kalkulator.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo $green"Game Janda......."
+sleep 3
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/3M0N27/GameJanda
+mv GameJanda$HOME
+cd GameJanda
+python2 tugas.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo $green"LiteSPAM......"
+sleep 3
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+mv LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo $red"SANTET..........."
+sleep 3
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+mv santet-online$HOME
+cd santet-online
+python2 santet.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+echo
+toilet -f standard "Mr. 3M0N27" -F gay
+sleep 3
+echo
+echo $yellow"LiteDDOS..........."
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS$HOME
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 0]
+then
+clear
+echo $red"Yakin mau keluar..?"
+sleep 3
+echo $green"Seriusss nihhh.."
+sleep 3
+echo $purple"lahh yang bener.."
+sleep 3
+echo $yellow"Thanks udah mampir"
+sleep 3
+clear
+exit
+fi
